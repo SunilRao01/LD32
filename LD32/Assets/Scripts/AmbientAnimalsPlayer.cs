9241940de58e0e24cfa2c6b416121479c67f049d
Bryan Spahr <bspahr314@gmail.com>
2015-04-18 07:03:06 -0500

    ambient sfx example with code

---
+++ LD32/Assets/Scripts/AmbientAnimalsPlayer.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AmbientAnimalsPlayer : MonoBehaviour {
+
+	public AudioClip[] daySounds;
+	public AudioClip[] nightSounds;
+	public GameObject AmbientForestSource;
+	private AmbientForestToggle aft;
+	private float soundCounter;
+
+	// Use this for initialization
+	void Start () {
+		aft = AmbientForestSource.GetComponent<AmbientForestToggle>();
+		Random.seed = (int)System.DateTime.Now.Ticks;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		periodicSound();
+	}
+
+	public void periodicSound(){
+		int x = Random.Range(10,25);
+		soundCounter += Time.deltaTime;
+		if (soundCounter >= x) {
+			bool day = aft.isDay();
+
+			if(day == true){
+				int n = Random.Range(0,daySounds.Length);
+				GetComponent<AudioSource>().clip = daySounds[n];
+			} else {
+				int n = Random.Range(0,nightSounds.Length);
+				GetComponent<AudioSource>().clip = nightSounds[n];
+			}
+
+			GetComponent<AudioSource>().Play();
+			soundCounter = 0f;
+		}
+	}
+}
---
+++ LD32/Assets/Scripts/AmbientForestToggle.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class AmbientForestToggle : MonoBehaviour {
+	public bool day;
+	public AudioClip[] ambience;
+
+	// Use this for initialization
+	void Start () {
+		day = true;
+		GetComponent<AudioSource>().clip = ambience[0];
+		GetComponent<AudioSource>().Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (day == true) {
+			GetComponent<AudioSource>().clip = ambience [0];
+		} else {
+			GetComponent<AudioSource>().clip = ambience [1];
+		}
+
+		if (GetComponent<AudioSource> ().isPlaying == false) {
+			GetComponent<AudioSource>().Play();
+		}
+	}
+
+	public bool isDay(){
+		return day;
+	}
+}
