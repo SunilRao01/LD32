7eda810d82013c985258ef575685bcdfa0ae2b75
Sunil Rao <slysunil101@gmail.com>
2015-04-17 21:57:48 -0500

    Updated VC Unity settings

---
+++ LD32/Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour 
+{
+	public float movementForce;
+	public float maxMovementSpeed;
+
+	void Start () 
+	{
+	
+	}
+	
+	void Update () 
+	{
+	
+	}
+
+	void handleMovment()
+	{
+		Vector2 movementDirection = new Vector2();
+		movementDirection.x = Input.GetAxisRaw("Horizontal") * movementForce;
+		movementDirection.y = Input.GetAxisRaw("Vertical") * movementForce;
+
+	}
+}
