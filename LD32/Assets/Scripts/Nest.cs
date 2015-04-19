a764a7c6bd08532cf5c5e59112f8c74435f192f5
Sunil Rao <slysunil101@gmail.com>
2015-04-19 01:07:08 -0500

    Added scoring UI element

---
+++ LD32/Assets/Scripts/Nest.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Nest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.name == "Player")
+		{
+			// TODO: Make buttons visible
+
+		}
+	}
+}
