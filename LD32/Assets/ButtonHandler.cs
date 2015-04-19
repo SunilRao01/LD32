8165786e712377c2e414dd5137c12847758c1b14
IanSwift <iswift2@uic.edu>
2015-04-19 18:01:30 -0500

    menu added largely (needs how to play)

---
+++ LD32/Assets/ButtonHandler.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonHandler : MonoBehaviour {
+
+	public GameObject screen;
+	public GameObject text;
+	public GameObject button;
+
+	private bool viewingCredits = false;
+
+	public void Awake()
+	{
+		screen.SetActive (false);
+		text.SetActive (false);
+		button.SetActive (false);
+	}
+
+	public void OnPressStart()
+	{
+		if (!viewingCredits) {
+			Application.LoadLevel ("Loader");
+		}
+	}
+
+	public void OnPressBackToMenu()
+	{
+		if (viewingCredits) {
+			screen.SetActive (false);
+			text.SetActive (false);
+			button.SetActive (false);
+			viewingCredits = false;
+		}
+	}
+	public void OnPressHowToPlay()
+	{
+	}
+	public void OnPressCredits()
+	{
+		if (!viewingCredits) {
+			screen.SetActive (true);
+			text.SetActive (true);
+			button.SetActive (true);
+			viewingCredits = true;
+		}
+	}
+	public void OnPressQuit()
+	{
+		if (!viewingCredits) {
+			Application.Quit ();
+		}
+	}
+}
