e8a5fd3d0f94b1db06c93feb56ee3a810b0bbab8
IanSwift <iswift2@uic.edu>
2015-04-20 21:38:51 -0500

    game over scene

--- LD32/Assets/Scripts/DeerView.cs
+++ LD32/Assets/Scripts/DeerView.cs
@@ -5,17 +5,24 @@ using Hamelin;
 public class DeerView : MonoBehaviour {
 	private float currentHealth = 50;
 
+	public Canvas deathView;
+
+	void Start()
+	{
+		deathView.enabled = false;
+	}
+
 	public void takeDamage(float damage)
 	{
 		currentHealth -= damage;
-		if (currentHealth <= 0) {
+		if (currentHealth <= 0) {	
 			GameObject player = Camera.main.GetComponentInChildren<GlobalView>().player;
 			GameObject.Destroy (player.GetComponent<Player>().canvas);
 			GameObject.Destroy(player);
 			GameObject.Destroy (GameObject.Find ("RegionContainer 1"));
 			GameObject.Destroy (GameObject.Find ("BG Music"));
 			GameObject.Destroy (gameObject);
-			Application.LoadLevel ("Title");
+			Application.LoadLevel ("GameOver");
 		}
 	}
 }
